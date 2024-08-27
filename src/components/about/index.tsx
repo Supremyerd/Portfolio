4a514d80f3da2ff82a16eb7853a34078f6500ebf
Supremyerd <andrey.zeferino@outlook.com>
2024-08-27 14:16:09 -0300

    left menu interactions

---
+++ src/components/about/index.tsx
@@ -0,0 +1,8 @@
+function AboutMe() {
+    return <div style={{position: 'absolute', left: '50vw', display:'grid', placeItems: 'center'}}>
+        About me
+    </div>
+
+}
+
+export default AboutMe;
--- src/components/left-menu/index.tsx
+++ src/components/left-menu/index.tsx
@@ -1,24 +1,33 @@
+import { pages } from '../../types';
 import './index.scss';
 import { GitHub, MenuBook, Handyman, ReceiptLong} from '@mui/icons-material';
 
-function LeftMenu() {
+type props = {
+    selectButton :pages,
+    changePage :(page :pages) => void
+}
+
+function LeftMenu({selectButton, changePage} :props) {
+
     return <>
     <div className="menu-left">
         <div className="icon">
             
         </div>
-        <div className="select">
+        <div className={selectButton == 'about_me' ? 'select' : 'btn'} onClick={() => changePage('about_me')}>
             <MenuBook className='btn-icon'/>
         </div>
-        <div className="btn">
+        <div className={selectButton == 'projects' ? 'select' : 'btn'} onClick={() => changePage('projects')}>
             <Handyman className='btn-icon'/>
         </div>
-        <div className="btn">
+        <div className={selectButton == 'certificates' ? 'select' : 'btn'} onClick={() => changePage('certificates')}>
             <ReceiptLong className='btn-icon'/>
         </div>
-        <div className="btn">
-            <GitHub className='btn-icon'/>
-        </div>
+        {/* <a href='https://github.com/Supremyerd'> */}
+            <a href='https://github.com/Supremyerd' className={selectButton == 'github' ? 'select' : 'btn'} onClick={() => changePage('github')}>
+                <GitHub className='btn-icon'/>
+            </a>
+        {/* </a> */}
     </div>
     </>
 }
