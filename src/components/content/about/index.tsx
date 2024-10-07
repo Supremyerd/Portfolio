0d8ec7e57b586ae7784e26663d7598d2b50aae49
Supremyerd <andrey.zeferino@outlook.com>
2024-10-07 10:19:16 -0300

    carousel about me page

--- src/components/content/about/index.tsx
+++ src/components/content/about/index.tsx
@@ -6,11 +6,12 @@ function AboutMe() {
       <div className="title">
           Andrey Henrique Lopes Zeferino
       </div>
+
       <div className='content middle'>
         <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam facilisis eu eros ac commodo. In euismod tristique dolor sed sollicitudin. Maecenas consectetur cursus efficitur. Nam porttitor risus nibh. Cras ac sagittis ante. Suspendisse quis pharetra velit. Fusce sed lorem sit amet lacus vehicula feugiat eget vitae odio. Duis at ex dictum, porttitor libero sed, mollis mi. Praesent venenatis ante vitae justo semper semper.
         </p>
-<p>
+    <p>
       Nulla aliquam, risus nec condimentum rhoncus, diam magna feugiat lectus, euismod hendrerit tortor lorem id nisl. Nunc vel diam bibendum, viverra nunc egestas, eleifend tellus. Nulla eget ante pellentesque, aliquam nisi ut, consequat neque. Mauris tincidunt eleifend lacus, varius pulvinar eros rhoncus et. Aliquam pretium ipsum ut justo mollis accumsan. Proin vehicula tincidunt consectetur. Phasellus eleifend vulputate nisi, sed bibendum urna iaculis non. In hac habitasse platea dictumst. Phasellus rutrum laoreet tellus consequat aliquet.
       </p>
       <p>
@@ -23,9 +24,82 @@ function AboutMe() {
       Donec tempor ligula neque, auctor cursus purus pulvinar in. Mauris a ornare risus, et elementum nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam eget augue suscipit, tincidunt ipsum sit amet, tincidunt nibh. Proin venenatis eros lorem, ut iaculis enim lacinia quis. Phasellus fermentum eu sapien id porta. Quisque in hendrerit ligula, vel lacinia lorem. Etiam mattis dolor eu convallis mollis. Nullam at mauris eros. Sed id ante id tortor vestibulum aliquam. Donec quis nibh fermentum, posuere mauris sit amet, egestas diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur faucibus ullamcorper purus, vel venenatis velit finibus id.
       </p>
       </div>
-      {/* <div className='bottom-content'>
-a
-      </div> */}
+
+      <div className='content bottom carrousel'>
+        {/*UncontrolledExample()*/}
+        <div id="carouselExampleCaptions" className="carousel slide">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="public/3.png" className="d-block w-100" alt="..."/>
+              {/* <div className="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div> */}
+            </div>
+            <div className="carousel-item">
+              <img src="public/2.png" className="d-block w-100" alt="..."/>
+              {/* <div className="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div> */}
+            </div>
+            <div className="carousel-item">
+              <img src="public/1.png" className="d-block w-100" alt="..."/>
+              {/* <div className="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+              </div> */}
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+        {/* <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="public/teste_002.jpg" className="d-block" alt="..."/>
+              <div className="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div>
+
+            </div>
+            <div className="carousel-item">
+              <img src="public/teste_001.jpg" className="d-block" alt="..."/>
+              <div className="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src="public/teste.jpg" className="d-block" alt="..."/>
+              <div className="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div> */}
+      </div>
     </>
 
 }
